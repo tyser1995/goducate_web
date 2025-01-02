06aa2130ba95ea4cc9788f06807c66e63ded9dc0
Dev_Resty <restygalido05@gmail.com>
2025-01-02 15:48:50 +0800

    package json and webpack

--- webpack.mix.js
+++ webpack.mix.js
@@ -18,9 +18,10 @@ mix.js('resources/js/app.js', 'public/js')
     .scripts(['resources/js/tagsinput.js'], 'public/js/custom.js')
     .sass('resources/sass/app.scss', 'public/css')
     .sourceMaps()
-    .copyDirectory('vendor/tinymce/tinymce', 'public/js/tinymce')
+    //.copyDirectory('vendor/tinymce/tinymce', 'public/js/tinymce')
     .copy('node_modules/html5-qrcode/html5-qrcode.min.js', 'public/js/html5-qrcode.min.js')
-    .copy('node_modules/print-js/dist/print.js', 'public/js/print.js');
+    .copy('node_modules/print-js/dist/print.js', 'public/js/print.js')
+    .copy('node_modules/tinymce', 'public/js/tinymce');
 
 var browser_sync = process.env.BROWSER_SYNC;
 
