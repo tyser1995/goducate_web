333c184e11402b7cb203a8cff0be3ef59b2f6ee9
lordtensu <louvincedigs@gmail.com>
2025-01-05 21:26:13 +0800

    updates

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -36,15 +36,7 @@
                         <a href="{{route('register')}}" class="text-center" hidden>Register a new membership</a>
                     </p>
                 </div>
-                <button type="submit" class="btn btn-primary btn-block">Sign In</button>
-                <div class="social-auth-links text-center mt-2 mb-3">
-                    <a href="#" class="btn btn-block btn-primary d-none">   
-                        <i class="fab fa-facebook mr-2"></i> Sign in using Facebook
-                    </a>
-                    <a href="{{ route('google.redirect') }}" class="btn btn-block btn-danger">
-                        <i class="fab fa-google-plus mr-2"></i> Login with Google
-                    </a>
-                </div>
+                <button type="submit" class="btn btn-primary btn-block mb-3">Sign In</button>
                 <div style="display: flex;
                 justify-content: space-between;">
                     <p>
